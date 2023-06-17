85c9727a740639bfbb7975dd0d48a0cc0a8b6513
Xuan045 <judychou60@gmail.com>
2023-06-17 13:00:18 +0800

    Modify png settings

--- af_compare/af_plot/af_compare.R
+++ af_compare/af_plot/af_compare.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(ggplot2)
 library(Cairo)
 
-setwd("/staging/biology/u4432941/dragen_accuracy/af_compare/af_plot")
+setwd("/staging/biology/u4432941/dragen_joint_calling/af_compare/af_plot")
 file <- "/staging/biology/u4432941/dragen_accuracy/af_compare/chr1.hg38_multianno.txt"
 
 df <- read.delim(file, sep = '\t')
@@ -14,8 +14,8 @@ scatter_plot <- function(input_df){
   # r^2: removing NA and using Pearson's correlation
   cor <- format(cor(as.numeric(input_df$AF_eas), as.numeric(input_df$Otherinfo1), 
                     method = "pearson", use = "complete.obs"), digits = 3)
-  # Number of variants
-  var_count <- nrow(input_df[input_df$Otherinfo1 != 0,])
+  # Number of variants, add comma as a thousands separator
+  var_count <- format(nrow(input_df[input_df$Otherinfo1 != 0,]), big.mark = ',')
   
   scatter_plot <- ggplot(data = input_df, aes(x = as.numeric(AF_eas), y = as.numeric(Otherinfo1))) +
     geom_point() +
@@ -23,13 +23,13 @@ scatter_plot <- function(input_df){
     theme_bw() +
     xlim(c(0, 1)) + ylim(c(0, 1)) +
     labs(x = "gnomAD_eas", y = "TWB1492_DRAGEN", 
-         subtitle = paste("Number of variants:", var_count, "\nr^2 =", cor))
+         subtitle = bquote("Number of variants:" ~ .(var_count) ~ ", " ~ r^2 ~ "=" ~ .(cor)))
   return(scatter_plot)
 }
 
 af_plot <- scatter_plot(df)
 
-# Save the plot as PNG using cairo_png device
-png("chr1_af.png", width = 6, height = 5, units = "in", res = 500, type = "cairo")
+# Save the plot as PNG using cairo device
+png("chr1_af.png", width = 11.6, height = 6.8, units = "in", res = 500, type = "cairo")
 print(af_plot)
 dev.off()
